ea817d85b8c1f9457eed73b25f9b856cf8ed6da4
joshuaAyodele <jayodele64@gmail.com>
2024-06-24 23:29:40 +0100

    change dbconnection to async await

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -5,16 +5,16 @@ const {
   dbName,
 } = require("./getEnvironmentVariables");
 
-const dbConnection = (cb) => {
-  mongoose
-    .connect(mongodbConnectionString, { dbName })
-    .then(() => {
-      cb();
-    })
-    .catch((err) => {
-      console.log("Cannot connect to db");
-      console.log(err);
+const dbConnection = async (cb) => {
+  try {
+    await mongoose.connect(mongodbConnectionString, {
+      dbName,
     });
+    cb();
+  } catch (err) {
+    console.log("Cannot connect to db");
+    console.log(err);
+  }
 };
 
 module.exports = dbConnection;
